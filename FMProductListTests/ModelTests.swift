6b80ff822f99a9d35b5d9f8873414d6dbba95fce
Rachel Radford <rachelaradford21@gmail.com>
2024-11-02 19:36:52 -0400

    added tests for model

---
+++ FMProductListTests/ModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  ModelTests.swift
+//  FMProductListTests
+//
+//  Created by Rachel Radford on 11/2/24.
+//
+
+import Testing
+
+struct ModelTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
