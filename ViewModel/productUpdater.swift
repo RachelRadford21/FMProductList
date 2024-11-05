c8d07ad0163b9f82f5e94cd7440218f93a11c9b3
RachelRadford21 <rachelaradford21@gmail.com>
2024-11-04 20:47:54 -0500

    `cleanup views and some data

---
+++ ViewModel/productUpdater.swift
@@ -0,0 +1,13 @@
+//
+//  productUpdater.swift
+//  FMProductList
+//
+//  Created by Rachel Radford on 11/4/24.
+//
+
+import Foundation
+
+class productUpdater: ObservableObject {
+  @Published var count: Int = 0
+
+}
