482c907c0c5a89133cefe1e0502f9274e74842f3
Rachel Radford <rachelaradford21@gmail.com>
2024-12-07 18:11:47 -0500

    add confirmation sheet

---
+++ View/GlobalViews/CardBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  CardBackgroundView.swift
+//  FMProductList
+//
+//  Created by Rachel Radford on 12/7/24.
+//
+
+import SwiftUI
+
+struct CardBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardBackgroundView()
+}
